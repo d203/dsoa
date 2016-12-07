2d6ad280d49b735857caec951dd374d2ee83a3ae
morland <mqy_bs@hotmail.com>
2016-12-08 04:29:55 +0800

    add test floder

---
+++ test/reqeust.py
@@ -0,0 +1,10 @@
+import requests
+import json
+ip="http://127.0.0.1:8080/addService"
+headers={'content-type':'application/json'}
+data={
+    'serviceName':'edge',
+    'serviceIP':'127.0.0.2',
+    'load':2
+    }
+requests.post(ip,data=json.dumps(data),headers=headers)
