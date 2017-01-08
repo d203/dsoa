34b616dd98c15fa61298f3cae925b8a8b31ec1a8
Wang Bingyi <1063091868@qq.com>
2017-01-08 01:43:47 -0800

    Wang Bingyi change html files and add upload file function

--- serverNode.py
+++ serverNode.py
@@ -16,24 +16,89 @@ global r
 
 @app.route('/',methods=['GET'])
 def index():
-    return render_template('index.html')
+    return render_template('AdminLTE-2.3.7/index.html')
+
+@app.route('/upload_files')
+def upload_files():
+    return render_template('AdminLTE-2.3.7/upload_files.html')
+
+@app.route('/upload_codes')
+def upload_codes():
+    return render_template('AdminLTE-2.3.7/upload_codes.html')
+
 @app.route('/status',methods=['GET'])
 def status():
     return 'ture'
+
 @app.route('/asyn')
 def wait():
     for i in range(1,100000000):
         pass
     return 'finish'
-#upload data to datacenter
+
+#------------------------------------------------------------------------------
+#Author : Meng Qingyu
+#Changer : Wang Bingyi
+#Data : 170108
+#Description : if post filename,then rename the filename ,else reserve it's
+#original name
+#------------------------------------------------------------------------------
+
 @app.route('/data/package/<filename>', methods=[ 'POST'])
 def upload_package_to_datacenter(filename):
     file = request.files['file']
     if file:
-        file.save(os.path.join('datacenter/data/package', filename))
+        if len(request.form['filename']) > 0:
+            file.save(os.path.join('datacenter/data/package', request.form['filename']))
+        file.save(os.path.join('datacenter/data/package', file.filename))
         return 'upload done'
     return 'upload fail'
+#------------------------------------------------------------------------------
+#Author : Wang Bingyi
+#Data : 170108
+#Description : Controller ,upload codes
+#------------------------------------------------------------------------------
 
+
+
+#------------------------------------------------------------------------------
+#Author : Wang Bingyi
+#Data : 170108
+#Description : Use redirect method to static files . But now ,we don't want to
+# use it ,so you can delete it! :)
+#------------------------------------------------------------------------------
+
+
+
+@app.route('/bootstrap/<s_path>')
+def static_path_bootstrap(s_path):
+    return redirect("/static/bootstrap/"+s_path)
+
+@app.route('/img/<s_path>')
+def static_path_img(s_path):
+    return redirect("/static/img/"+s_path)
+
+@app.route('/build/<s_path>')
+def static_path_build(s_path):
+    return redirect("/static/build/"+s_path)
+
+@app.route('/dist/img/<s_path>')
+def static_path_dist(s_path):
+    return redirect("/static/dist/img/"+s_path)
+
+@app.route('/documentation/<s_path>')
+def static_path_documentation(s_path):
+    return redirect("/static/documentation/"+s_path)
+
+@app.route('/pages/<s_path>')
+def static_path_pages(s_path):
+    return redirect("/static/pages/"+s_path)
+
+@app.route('/plugins/<s_path>')
+def static_path_plugins(s_path):
+    return redirect("/static/plugins/"+s_path)
+
+#---------------------------------End------------------------------------------
 
 # @app.route('/uploads/<filename>',methods=['GET'])
 # def uploaded_file(filename):
