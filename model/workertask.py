7b8040b83163b1f94c0fac2f59435958857b7b97
yubo <yubowu_do@hotmail.com>
2017-01-11 04:51:18 +0800

    complex the info page of TaskInfo

--- model/workertask.py
+++ model/workertask.py
@@ -5,11 +5,12 @@ class WorkerTask(models.Model):
     worker_name=models.Attribute(required=True)
     script_code=models.Attribute(required=True)
     status=models.Attribute()
-    
+    file_package=models.Attribute()
     def get_json(self):
         return json.dumps({
             'task_id':self.task_id,
             'worker_name':self.worker_name,
             'script_code':self.script_code,
-            'status':self.status
+            'status':self.status,
+            'file_package':self.file_package
         })
